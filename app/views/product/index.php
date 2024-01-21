e6c8412638e08e1e5ff3792a9a8672be017734f4
WKolasaa <wkolasaa@gmail.com>
2024-01-21 05:11:58 +0100

    basketLogic

--- app/views/product/index.php
+++ app/views/product/index.php
@@ -49,33 +49,7 @@ require_once __DIR__ . '/../shared/header.php';
     </p>
 </div>
 
-<script>
-    function addToBasket(productName, productPrice) {
-        const basketList = document.getElementById('basketList');
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
-        listItem.textContent = `${productName} - ${productPrice}`;
-
-        // Add a remove button to each item
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        removeButton.classList.add('btn', 'btn-danger', 'btn-sm', 'float-right', 'ml-2');
-        removeButton.onclick = function() {
-            listItem.remove();
-        };
-
-        listItem.appendChild(removeButton);
-        basketList.appendChild(listItem);
-    }
-
-    function finalizeOrder() {
-        // Implement logic to finalize the order
-        // For example, redirect to the order page
-        window.location.href = '/order';
-    }
-</script>
-
-<!--<script src="basketLogic.js"></script>-->
+<script src="basketLogic.js"></script>
 
 </body>
 </html>
